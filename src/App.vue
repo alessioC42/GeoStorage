637ff5a8acc7818571518847f96ef14f2f185021
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2024-04-01 22:57:12 +0200

    Purge default project to minimum and add dependencies

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-app>
+    <h1>Hello World</h1>
+    <v-btn>Hello Vuetify</v-btn>
+  </v-app>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+//map styles
+import 'leaflet/dist/leaflet.css'
+import 'vue-map-ui/dist/normalize.css'
+import 'vue-map-ui/dist/style.css'
+import 'vue-map-ui/dist/theme-all.css'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
+createApp(App).use(vuetify).mount('#app')
